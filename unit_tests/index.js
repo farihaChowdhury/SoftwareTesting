9eab5b92db737e4b12310088a38ca75ad3660034
Fariha Chowdhury <fariha.chowdhury@tuni.fi>
2022-12-18 15:36:00 +0200

    added unit tests

--- unit_tests/index.js
+++ unit_tests/index.js
@@ -10,12 +10,86 @@ import toInteger from '../components/toInteger.js';
 import toNumber from '../components/toNumber.js';
 import toString from '../components/toString.js';
 
+
+const users = [
+     { 'user': 'barney', 'active': true },
+     { 'user': 'betty', 'active': true },
+     { 'user': 'fred', 'active': false }
+  ]
+
 describe('Add', () => {
   describe('Add numbers', function () {
     it('must return sum of two numbers', () => {
       expect(4).to.be.deep.equal(add(3, 1));
     });
   });
+
+  describe('Add numbers', function () {
+    it('must return sum of one positive and one negative number', () => {
+      expect(-10).to.be.deep.equal(add(-15, 5));
+    });
+  });
+
+  describe('Add numbers', function () {
+    it('must return sum of fraction numbers', () => {
+      expect(5.5).to.be.deep.equal(add(2, 3.5));
+    });
+  });
+
+});
+
+describe('isDate', () => {
+  describe('Check Date', function () {
+    it('must return true if date object', () => {
+      expect(true).to.be.equal(isDate(new Date()));
+    });
+  });
+
+  describe('Check Invalid Date', function () {
+    it('must return false if not date object', () => {
+      expect(false).to.be.equal(isDate('Mon April 23 2012'));
+    });
+  });
+});
+
+describe('isEmpty', () => {
+  describe('Check if empty for null', function () {
+    it('must return true if null', () => {
+      expect(true).to.be.equal(isEmpty(null));
+    });
+  });
+
+  describe('Check if empty for string', function () {
+    it('must return false if not null', () => {
+      expect(false).to.be.equal(isEmpty('abc'));
+    });
+  });
+  describe('Check if empty for boolean value', function () {
+    it('must return true if value is boolean', () => {
+      expect(true).to.be.equal(isEmpty(true));
+    });
+  });
+});
+
+describe('filter', () => {
+  describe('Get all Active users', function () {
+    it('must return users who are active', () => {
+      var filt = users.filter(function(item) {
+        return item.active == true;
+      });
+      expect(filt).to.be.equal(filter(users, ({ active }) => active));
+    });
+  });
+
+});
+
+describe('countBy', () => {
+  describe('Check if empty for null', function () {
+    it('must return true if null', () => {
+      expect(true).to.be.equal(isEmpty(null));
+    });
+  });
+
 });
 
 describe('toInteger', () => {
